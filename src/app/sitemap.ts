09185ee303d5fd402732f70151c7c03b46e9261f
jamesshealy109-sudo <jamesshealy109@gmail.com>
2026-09-08 17:59:27 -0400

    Make sitemap metadata static-export compatible

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,9 @@
 import type { MetadataRoute } from "next";
 import { allPages } from "@/content/pages";
 import { config } from "@/lib/config";
+
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     "/",
