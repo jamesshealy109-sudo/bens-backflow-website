7da59b516d4812ed2ead16cdb0486c4cea069c56
jamesshealy109-sudo <jamesshealy109@gmail.com>
2026-09-08 17:59:21 -0400

    Make robots metadata static-export compatible

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,5 +1,8 @@
 import type { MetadataRoute } from "next";
 import { config } from "@/lib/config";
+
+export const dynamic = "force-static";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
